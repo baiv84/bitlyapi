7f2efc7aa624cf945cee0eb3bd0247d1b8dac1ba
baiv84 <iambabintsev@gmail.com>
2023-01-16 18:04:06 +0700

    # Commit - Add token

--- bitlyapi.py
+++ bitlyapi.py
@@ -1,5 +1,14 @@
-#coding: utf-8
+import requests
+
+headers = {
+    'Authorization': 'Bearer b502a1b1722ba710e108143556c15b78bc0b78e7',
+    'Content-Type': 'application/json',
+}
+
+data = '{ "long_url": "https://openbsd.org/", "domain": "bit.ly" }'
 
 
-if __name__ == '__main__':
-    print('bitly API')
+
+response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+print(response.status_code)
+print(response.text)
