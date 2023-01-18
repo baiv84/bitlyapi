f88dbe892b738cb66079f0fee80481ac30a7e2dc
baiv84 <iambabintsev@gmail.com>
2023-01-18 17:50:33 +0700

    # Commit - add crypto procedure for bitly token

--- bitlyapi.py
+++ bitlyapi.py
@@ -1,14 +1,17 @@
 import requests
 
+
+CRYPTED_TOKEN = '98:53:48:50:97:49:98:49:55:50:50:98:97:55:49:48:101:49:48:56:49:52:51:53:53:54:99:49:53:98:55:56:98:99:48:98:55:56:101:55'
+DECRYPTED_TOKEN = "".join([chr(int(c)) for c in CRYPTED_TOKEN.split(':')])
+
+
 headers = {
-    'Authorization': 'Bearer b502a1b1722ba710e108143556c15b78bc0b78e7',
+    'Authorization': 'Bearer %s' % (DECRYPTED_TOKEN),
     'Content-Type': 'application/json',
 }
 
-data = '{ "long_url": "https://openbsd.org/", "domain": "bit.ly" }'
 
-
-
+data = '{ "long_url": "https://openwall.org/", "domain": "bit.ly" }'
 response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
 print(response.status_code)
 print(response.text)
