fdb4eabdb425e5ce01d5bfdce60a034fe87034c3
baiv84 <iambabintsev@gmail.com>
2023-01-18 22:00:08 +0700

    # Rebuild code structure - clean garbage

--- bitlyapi.py
+++ bitlyapi.py
@@ -1,10 +1,9 @@
-import re
-from tools.url_shrinker import URLShrinker
-
+import requests
 
 CRYPTED_TOKEN = '98:53:48:50:97:49:98:49:55:50:50:98:97:55:49:48:101:49:48:56:49:52:51:53:53:54:99:49:53:98:55:56:98:99:48:98:55:56:101:55'
 DECRYPTED_TOKEN = "".join([chr(int(c)) for c in CRYPTED_TOKEN.split(':')])
 
+print(DECRYPTED_TOKEN)
 
 headers = {
     'Authorization': 'Bearer %s' % (DECRYPTED_TOKEN),
